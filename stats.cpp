f2baba1ff7398fb140e711ac8f2fa78193506fcd
WardaGul56 <warda42gul@gmail.com>
2025-04-14 09:20:45 -0700

    Add files via upload

---
+++ stats.cpp
@@ -0,0 +1,36 @@
+class stats
+{
+    private:
+      int current_hp;
+      int damage;
+
+    public:
+      stats ( int chp , int d)
+      {
+        current_hp = chp;
+        damage = d;
+      }
+      void set_hp ( int chp)
+      {
+         if (chp <= 0) { cout << "You are out of hp loserr!!"; }
+         else  { current_hp = chp; }
+      }
+     
+      int get_chp() { return current_hp; }
+      
+      int increase_hp (int i_hp)
+      {
+        current_hp = current_hp + i_hp;
+      }
+
+      int decrease_hp (int d_hp )
+      {
+        current_hp = current_hp - d_hp;
+      }
+
+      void set_damage ( int d ) { damage = d; }
+      int get_damage ()  {return damage ; }
+
+      int increase_damage (int inc_d)  { damage = damage + inc_d; }
+      int decrease_damage (int dec_d)  { damage = damage - dec_d; }
+};
